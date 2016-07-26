eee57b21c3700244801049afa8ba5c6019effa95
Cameron Dawson <cdawson@mozilla.com>
2016-07-26 15:09:55 -0700

    Bug 1264074 - Add ability to ingest Mercurial Resultsets via Pulse

--- tests/etl/test_resultset_loader.py
+++ tests/etl/test_resultset_loader.py
@@ -6,6 +6,7 @@ import responses
 
 from treeherder.etl.resultset_loader import (GithubPullRequestTransformer,
                                              GithubPushTransformer,
+                                             HgPushTransformer,
                                              ResultsetLoader)
 
 
@@ -20,6 +21,11 @@ def github_pr(sample_data):
 
 
 @pytest.fixture
+def hg_push(sample_data):
+    return copy.deepcopy(sample_data.hg_push)
+
+
+@pytest.fixture
 def transformed_github_push(sample_data):
     return copy.deepcopy(sample_data.transformed_github_push)
 
@@ -27,6 +33,11 @@ def transformed_github_push(sample_data):
 @pytest.fixture
 def transformed_github_pr(sample_data):
     return copy.deepcopy(sample_data.transformed_github_pr)
+
+
+@pytest.fixture
+def transformed_hg_push(sample_data):
+    return copy.deepcopy(sample_data.transformed_hg_push)
 
 
 @pytest.fixture
@@ -59,6 +70,21 @@ def mock_github_push_commits(activate_responses):
                   content_type='application/json')
 
 
+@pytest.fixture
+def mock_hg_push_commits(activate_responses):
+    tests_folder = os.path.dirname(os.path.dirname(__file__))
+    path = os.path.join(
+        tests_folder,
+        "sample_data/pulse_consumer",
+        "hg_push_commits.json"
+    )
+    with open(path) as f:
+        mocked_content = f.read()
+    responses.add(responses.GET, "https://hg.mozilla.org/try/json-pushes",
+                  body=mocked_content, status=200, match_querystring=False,
+                  content_type='application/json')
+
+
 @pytest.mark.parametrize("exchange, transformer_class", [
     ("exchange/taskcluster-github/v1/push", GithubPushTransformer),
     ("exchange/taskcluster-github/v1/pull-request", GithubPullRequestTransformer)])
@@ -67,15 +93,24 @@ def test_get_transformer_class(exchange, transformer_class):
     assert rsl.get_transformer_class(exchange) == transformer_class
 
 
-def test_ingest_github_pull_request(jm, github_pr, transformed_github_pr,
-                                    mock_github_pr_commits):
+def test_ingest_pulse_github_pull_request(jm, github_pr, transformed_github_pr,
+                                          mock_github_pr_commits):
     xformer = GithubPullRequestTransformer(github_pr)
     resultset = xformer.transform(jm.project)
     assert transformed_github_pr == resultset
 
 
-def test_ingest_github_push(jm, github_push, transformed_github_push,
-                            mock_github_push_commits):
+def test_ingest_pulse_github_push(jm, github_push, transformed_github_push,
+                                  mock_github_push_commits):
     xformer = GithubPushTransformer(github_push)
     resultset = xformer.transform(jm.project)
     assert transformed_github_push == resultset
+
+
+def test_ingest_pulse_hg_push(jm, hg_push, transformed_hg_push,
+                              mock_hg_push_commits):
+    xformer = HgPushTransformer(hg_push)
+    resultset = xformer.transform(jm.project)
+    import pprint
+    pprint.pprint(resultset)
+    assert transformed_hg_push == resultset
--- treeherder/etl/resultset_loader.py
+++ treeherder/etl/resultset_loader.py
@@ -31,7 +31,7 @@ class ResultsetLoader:
 
         except ObjectDoesNotExist:
             newrelic.agent.record_custom_event("skip_unknown_repository",
-                                               message_body["details"])
+                                               transformer.repo_url)
             logger.warn("Skipping unsupported repo: {}".format(
                 transformer.repo_url))
         except Exception as ex:
@@ -44,6 +44,8 @@ class ResultsetLoader:
                 return GithubPushTransformer
             elif exchange.endswith("pull-request"):
                 return GithubPullRequestTransformer
+        elif "hgpushes" in exchange:
+            return HgPushTransformer
         raise PulseResultsetError(
             "Unsupported resultset exchange: {}".format(exchange))
 
@@ -180,6 +182,77 @@ class GithubPullRequestTransformer(GithubTransformer):
         )
 
         return self.fetch_resultset(pr_url, repository)
+
+
+class HgPushTransformer:
+    """
+    {
+      "root": {
+        "payload": {
+          "pushlog_pushes": [
+            {
+              "time": 14698302460,
+              "push_full_json_url": "https://hg.mozilla.org/try/json-pushes?version=2&full=1&startID=136597&endID=136598",
+              "pushid": 136598,
+              "push_json_url": " https: //hg.mozilla.org/try/json-pushes?version=2&startID=136597&endID=136598",
+              "user": " james@hoppipolla.co.uk"
+            }
+          ],
+          "heads": [
+            "2f77bc4f354d9ba67ea5270b2fc789f4b0521287"
+          ],
+          "repo_url": "https://hg.mozilla.org/try",
+          "_meta": {
+            "sent": "2016-07-29T22:11:18.503365",
+            "routing_key": "try",
+            "serializer": "json",
+            "exchange": "exchange/hgpushes/v1"
+          }
+        }
+      }
+    }
+    """
+    def __init__(self, message_body):
+        self.message_body = message_body
+        self.repo_url = message_body["payload"]["repo_url"]
+        self.branch = None
+
+    def transform(self, repository):
+        logger.info("transforming for {}".format(repository))
+        url = self.message_body["payload"]["pushlog_pushes"][0]["push_full_json_url"]
+        return self.fetch_resultset(url, repository)
+
+    def fetch_resultset(self, url, repository, sha=None):
+        try:
+            logger.info("fetching for {} {}".format(repository, url))
+            # there will only ever be one, with this url
+            push = fetch_json(url)["pushes"].values()[0]
+            commits = list(push["changesets"])
+            head_commit = commits[0]
+            logger.info("got head commit: {}".format(head_commit["node"]))
+            resultset = {
+                "revision": head_commit["node"],
+                "push_timestamp": push["date"],
+                "author": push["user"],
+            }
+
+            revisions = []
+            for commit in commits:
+                revisions.append({
+                    "comment": commit["desc"],
+                    "repository": repository,
+                    "author": commit["author"],
+                    "revision": commit["node"]
+                })
+
+            resultset["revisions"] = revisions
+            return resultset
+
+        except Exception as ex:
+            logger.exception("Error fetching commits", exc_info=ex)
+            newrelic.agent.record_exception(ex, params={
+                "url": url, "repository": repository, "sha": sha
+                })
 
 
 class PulseResultsetError(ValueError):
